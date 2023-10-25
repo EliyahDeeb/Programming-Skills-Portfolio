8b6822d559d81709fdebd876b305c035d5446a29
Eliyah Deeb <144203274+EliyahDeeb@users.noreply.github.com>
2023-10-25 09:42:52 +0400

    Delete Programming directory

--- Programming/Chapter 1/Chap1_Ex2.py
+++
@@ -1,8 +0,0 @@
-#Exercise 2: Print Python Version
-
-#"sys" means 'System-specific parameters and functions', or System for short.
-import sys
-print("Python Version: ")
-print(sys.version) #"sys.version" shows the latest version of the System of Python.
-print("Python Version Info: ")
-print(sys.version_info) #"sys.version_info" shows the version info of Python's System.
--- Programming/Chapter 7/Chap7_Exercise2.py
+++
@@ -1,8 +0,0 @@
-#Exercise 2: Favorite Book
-
-
-def favorite_book(title):
-    print(title,"is one of my favorite books")
-    
-#The (title) part refers to the book's title.
-favorite_book('Alice in Wonderland')
