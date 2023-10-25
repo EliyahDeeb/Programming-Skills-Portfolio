9059119a204894122235c2cbdaa6cd85bfb25c0d
Eliyah Deeb <144203274+EliyahDeeb@users.noreply.github.com>
2023-10-25 10:15:35 +0400

    Delete Programming directory

--- Programming/Chapter 2/Chap2_Ex1.py
+++
@@ -1,9 +0,0 @@
-#Exercise 1: Variables
-
-#Variables are used for storing data by using a specific name, letter, number, etc.
-
-a="What do you do in your free time?"
-print(a)
-
-b=("I usually just do everyday chores, that’s all.")
-print(b)
--- Programming/Chapter 2/Chap2_Ex4.py
+++
@@ -1,9 +0,0 @@
-#Exercise 4: Favorite Number 
-#Use a variable to represent your favorite number.
-#Then,using that variable, create a message that reveals your favorite number.
-#Print that message.
-
-a = 19
-#In a 'print' command, the {Curly Brackets} are used for representing variables.
-msg = (f'My favorite number is the number {a}.')
-print(msg)
